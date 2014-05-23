2ad1c1360c355a476b37244d3a0643ba8455c53f
dili <dili.hu@atomicobject.com>
2014-05-23 14:20:54 -0400

    Initiate project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackRails::Application.config.secret_key_base = '3bc453a88805a9d649b6a0b5e8187062ccd6b5792c3b2f04f4b2dc3ccb50a9dc6fd6ef6aaff0c57b688aca718473fe736f3e24a45bbe017fe1dc9e7b1050bffd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackRails::Application.config.session_store :cookie_store, key: '_backRails_session'
